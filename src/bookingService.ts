6c6a19b82e656aadfa4cc85548d6e9eaa03af5b1
Nick <naumovn808@gmail.com>
2025-10-15 22:24:41 +0500

    feat: add endpoint counts

--- src/bookingService.ts
+++ src/bookingService.ts
@@ -1,16 +1,19 @@
-import pool from './db';
-import { type PoolClient, DatabaseError } from 'pg';
+import pool from "./db";
+import { type PoolClient, DatabaseError } from "pg";
 
 interface BookingData {
   event_id: number;
   user_id: string;
 }
 
-export async function reserveSeat({ event_id, user_id }: BookingData): Promise<number> {
+export async function reserveSeat({
+  event_id,
+  user_id,
+}: BookingData): Promise<number> {
   const client: PoolClient = await pool.connect();
 
   try {
-    await client.query('BEGIN');
+    await client.query("BEGIN");
 
     const eventResult = await client.query(
       `SELECT total_seats FROM events WHERE id = $1 FOR UPDATE`,
@@ -18,7 +21,7 @@ export async function reserveSeat({ event_id, user_id }: BookingData): Promise<n
     );
 
     if (eventResult.rows.length === 0) {
-      throw new Error('Event not found');
+      throw new Error("Event not found");
     }
     const totalSeats = eventResult.rows[0].total_seats;
 
@@ -29,8 +32,8 @@ export async function reserveSeat({ event_id, user_id }: BookingData): Promise<n
     const currentBookedCount = parseInt(bookingCountResult.rows[0].count, 10);
 
     if (currentBookedCount >= totalSeats) {
-      await client.query('ROLLBACK');
-      throw new Error('No available seats left');
+      await client.query("ROLLBACK");
+      throw new Error("No available seats left");
     }
 
     const insertResult = await client.query(
@@ -40,21 +43,19 @@ export async function reserveSeat({ event_id, user_id }: BookingData): Promise<n
 
     const newBookingId = insertResult.rows[0].id;
 
-    await client.query('COMMIT');
+    await client.query("COMMIT");
 
     return newBookingId;
-
   } catch (error) {
+    await client.query("ROLLBACK");
 
-    await client.query('ROLLBACK');
-
-    if (error instanceof DatabaseError && error.code === '23505') {
-        throw new Error('User has already booked a seat for this event');
+    if (error instanceof DatabaseError && error.code === "23505") {
+      throw new Error("User has already booked a seat for this event");
     }
 
     throw error;
-    
   } finally {
     client.release();
   }
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,84 @@
-import express, { type Request, type Response } from 'express';
-import { reserveSeat } from './bookingService';
-import dotenv from 'dotenv';
+import express, { type Request, type Response } from "express";
+import { reserveSeat } from "./bookingService";
+import dotenv from "dotenv";
+import  pool  from "./db"; //
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.post('/api/bookings/reserve', async (req: Request, res: Response) => {
+app.post("/api/bookings/reserve", async (req: Request, res: Response) => {
   const { event_id, user_id } = req.body;
 
   if (!event_id || !user_id) {
-    return res.status(400).json({ error: 'Missing event_id or user_id' });
+    return res.status(400).json({ error: "Missing event_id or user_id" });
   }
 
   try {
-    const bookingId = await reserveSeat({ 
-        event_id: parseInt(event_id), 
-        user_id 
+    const bookingId = await reserveSeat({
+      event_id: parseInt(event_id),
+      user_id,
     });
 
-    res.status(201).json({ 
-        message: 'Seat successfully reserved', 
-        booking_id: bookingId,
-        event_id,
-        user_id
+    res.status(201).json({
+      message: "Seat successfully reserved",
+      booking_id: bookingId,
+      event_id,
+      user_id,
     });
-
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-    
-    if (errorMessage === 'User has already booked a seat for this event') {
-      return res.status(409).json({ error: errorMessage }); 
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+
+    if (errorMessage === "User has already booked a seat for this event") {
+      return res.status(409).json({ error: errorMessage });
     }
-    if (errorMessage === 'No available seats left') {
-      return res.status(403).json({ error: errorMessage }); 
+    if (errorMessage === "No available seats left") {
+      return res.status(403).json({ error: errorMessage });
     }
-    if (errorMessage === 'Event not found') {
-        return res.status(404).json({ error: errorMessage }); 
+    if (errorMessage === "Event not found") {
+      return res.status(404).json({ error: errorMessage });
     }
 
-    console.error('Booking error:', error);
-    res.status(500).json({ error: 'Internal server error during booking' });
+    console.error("Booking error:", error);
+    res.status(500).json({ error: "Internal server error during booking" });
+  }
+});
+
+app.get("/api/get-user-count", async (req: Request, res: Response) => {
+  const { period } = req.query;
+
+  let interval = "1 month";
+  if (period === "day") interval = "1 day";
+  else if (period === "week") interval = "1 week";
+  else if (period === "month") interval = "1 month";
+
+  try {
+    const result = await pool.query(
+      `
+      SELECT 
+        user_id,
+        COUNT(*) AS booking_count
+      FROM bookings
+      WHERE created_at >= NOW() - INTERVAL '${interval}'
+      GROUP BY user_id
+      ORDER BY booking_count DESC
+      LIMIT 10;
+      `
+    );
+
+    res.json(
+      result.rows.map((row) => ({
+        user_id: row?.user_id,
+        booking_count: Number(row?.booking_count),
+      }))
+    );
+  } catch (e) {
+    console.error("error", e);
+    res.status(500).json({ error: "Internal server error", e });
   }
 });
 
